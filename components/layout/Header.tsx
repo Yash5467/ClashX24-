8deeb4207df46c273188e40a8df4bae48bde5484
Yashpal Chouhan <yashstar0009@gmail.com>
2025-04-01 15:57:59 +0530

    apk build updated

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -92,7 +92,7 @@ export default function Header() {
         </nav>
 {/* 
         <a
-          href='https://expo.dev/artifacts/eas/a5fiFWEN3ojHrbWoMcnoa.apk'
+          href='https://expo.dev/artifacts/eas/qsDfDUMeKy1uGgPoz4s7Zf.apk'
           download={true}
         >
           <Button variant="gaming" size="lg" className="gap-2">
@@ -136,7 +136,7 @@ export default function Header() {
               ))}
               <a
                 className="mt-8"
-                href='https://expo.dev/artifacts/eas/a5fiFWEN3ojHrbWoMcnoa.apk'
+                href='https://expo.dev/artifacts/eas/qsDfDUMeKy1uGgPoz4s7Zf.apk'
                 download={true}
               >
                 <Button variant="gaming" size="lg" className="gap-2">
