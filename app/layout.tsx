d1b5aaf18bbe96c551420b0d247209097ca85171
Yashpal Chouhan <yashstar0009@gmail.com>
2025-05-05 23:17:41 +0530

    google analytics tag added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type React from "react"
 import "@/app/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import './globals.css'
+import { TooltipProvider } from "@/components/ui/tooltip"
+import Script from "next/script"
 
 export const metadata = {
   title: "ClashX24 - BGMI & Free Fire Tournament Platform",
@@ -15,10 +18,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-       {/* <head>
-        <link rel="icon" href="/favicon.ico" />
-   
-      </head> */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-MBCJGBW7YG"
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-MBCJGBW7YG');
+        `}
+      </Script>
       <body className="min-h-screen bg-background font-sans antialiased">
         <TooltipProvider>
           <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
@@ -31,6 +42,3 @@ export default function RootLayout({
 }
 
 
-
-import './globals.css'
-import { TooltipProvider } from "@/components/ui/tooltip"
