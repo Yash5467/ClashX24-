059fda1f9e3eaf2504fc51e126335d9024aca219
Yashpal Chouhan <yashstar0009@gmail.com>
2025-06-12 14:25:51 +0530

    qr landing updated

--- screens/qr-landing.screen.tsx
+++ screens/qr-landing.screen.tsx
@@ -3,6 +3,8 @@
 import { useState, useRef, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Iphone15Pro from "@/components/magicui/iphone-15-pro";
 
 export default function QRLandingScreen() {
   const [started, setStarted] = useState(false);
@@ -29,26 +31,34 @@ export default function QRLandingScreen() {
 
   return (
     <div
-      className="h-screen w-full flex justify-center items-center"
+      className="h-screen w-full relative flex justify-center items-center px-4"
     >
       {!started && (
-        <Button
+        <button
+          className="text-xl px-0 py-0  absolute inset-0  h-full cursor-pointer"
           onClick={() => setStarted(true)}
         >
-          Tap to Start
-        </Button>
+          <Image
+            src="https://res.cloudinary.com/dqdvsab8g/image/upload/v1749717713/srnvzxz0snzehgz77yty.jpg"
+            alt="bg-image-qr"
+            height={800}
+            width={1600}
+            className="-z-10 opacity-50 h-screen cursor-pointer"
+          />
+        <span className="absolute top-1/2 left-1/2 font-semibold text-xl cursor-pointer -translate-x-1/2 -translate-y-1/2" >
+            Tap to Start
+        </span>
+        </button>
       )}
 
       {started && (
-        <video
-          ref={videoRef}
-          src="https://res.cloudinary.com/dqdvsab8g/video/upload/v1749713842/bmosckbtedhflavxrxla.mp4"
-          autoPlay
-          controls={false}
-          muted={false}
-          playsInline
-          className="h-full"
-        />
+        <div className="relative">
+          <Iphone15Pro
+            videRef={videoRef}
+            className="size-full"
+            videoSrc="https://res.cloudinary.com/dqdvsab8g/video/upload/v1749713842/bmosckbtedhflavxrxla.mp4"
+          />
+        </div>
       )}
     </div>
   );
